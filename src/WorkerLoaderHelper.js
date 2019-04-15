93de88101e15a3cc11f8787a7f36f8962ebfba23
Dario Segura <darionco.dev@gmail.com>
2019-04-15 19:16:19 -0400

    [NODE] Support Node.js

--- src/WorkerLoaderHelper.js
+++ src/WorkerLoaderHelper.js
@@ -4,6 +4,17 @@ export function createInlineWorkerFactory(fn, sourcemap = null) {
     const end = source.indexOf('}', source.length - 1);
     const body = source.substring(start, end) + (sourcemap ? `//# sourceMappingURL=${sourcemap}` : '');
     const lines = body.split('\n').map(line => line.substring(4) + '\n');
+
+    if (Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]') {
+        /* node.js */
+        const Worker = require('worker_threads').Worker;
+        const source = lines.join('\n');
+        return function WorkerFactory(options) {
+            return new Worker(source, Object.assign({}, options, { eval: true }));
+        };
+    }
+
+    /* browser */
     const blob = new Blob(lines, { type: 'application/javascript' });
     const url = URL.createObjectURL(blob);
     return function WorkerFactory(options) {
@@ -12,6 +23,14 @@ export function createInlineWorkerFactory(fn, sourcemap = null) {
 }
 
 export function createURLWorkerFactory(url) {
+    if (Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]') {
+        /* node.js */
+        const Worker = require('worker_threads').Worker;
+        return function WorkerFactory(options) {
+            return new Worker(url, options);
+        }
+    }
+    /* browser */
     return function WorkerFactory(options) {
         return new Worker(url, options);
     }
