c951f86917e07178602ae4f86d83bd4fe844067d
Dario Segura <darionco.dev@gmail.com>
2019-11-30 03:12:32 -0500

    [BUILD] Supports array/object file input

--- src/index.js
+++ src/index.js
@@ -34,7 +34,50 @@ module.exports = function workerLoaderPlugin(config = null) {
                         }
                     });
                     projectOptions.plugins = plugins;
-                    basePath = path.dirname(options.input);
+
+                    const cwd = process.cwd();
+                    if (typeof options.input === 'string') {
+                        try {
+                            const entry = require.resolve(options.input, {paths:[cwd]});
+                            basePath = path.dirname(entry);
+                        } catch (e) {}
+                    } else if (Array.isArray(options.input)) {
+                        let componentCount = Number.MAX_SAFE_INTEGER;
+                        let shortestPath = null;
+                        for (let i = 0,n = options.input.length; i < n; ++i) {
+                            try {
+                                const entry = require.resolve(options.input[i], {paths:[cwd]});
+                                const entryPath = path.dirname(entry);
+                                const components = entryPath.split(path.sep);
+                                if (components.length < componentCount) {
+                                    componentCount = components.length;
+                                    shortestPath = entryPath;
+                                }
+                            } catch (e) {}
+                        }
+                        basePath = shortestPath;
+                    } else {
+                        const keys = Object.keys(options.input);
+                        let componentCount = Number.MAX_SAFE_INTEGER;
+                        let shortestPath = null;
+                        for (let i = 0, n = keys.length; i < n; ++i) {
+                            const input = options.input[keys[i]];
+                            try {
+                                const entry = require.resolve(input, {paths:[cwd]});
+                                const entryPath = path.dirname(entry);
+                                const components = entryPath.split(path.sep);
+                                if (components.length < componentCount) {
+                                    componentCount = components.length;
+                                    shortestPath = entryPath;
+                                }
+                            } catch (e) {}
+                        }
+                        basePath = shortestPath;
+                    }
+
+                    if (!basePath) {
+                        basePath = '.';
+                    }
                 }
             }
 
