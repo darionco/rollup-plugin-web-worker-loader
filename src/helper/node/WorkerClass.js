818b73e01101ae2258c27368c34285810bcf3928
Dario Segura <darionco.dev@gmail.com>
2020-05-01 10:14:25 -0400

    [WEBPACK] Avoid warning on certain webpack builds. Fixes #19

--- src/helper/node/WorkerClass.js
+++ src/helper/node/WorkerClass.js
@@ -1,13 +1,11 @@
-var kRequire =
-    typeof module !== 'undefined' && typeof module.require === 'function' && module.require || // eslint-disable-line
-    typeof __non_webpack_require__ === 'function' && __non_webpack_require__ || // eslint-disable-line
-    typeof require === 'function' && require || // eslint-disable-line
-    null; // eslint-disable-line
-
 var WorkerClass = null;
 
-if (kRequire) {
-    try { WorkerClass = kRequire('worker_threads').Worker; } catch(e) {} // eslint-disable-line
-}
+try {
+    var WorkerThreads =
+        typeof module !== 'undefined' && typeof module.require === 'function' && module.require('worker_threads') ||
+        typeof __non_webpack_require__ === 'function' && __non_webpack_require__('worker_threads') ||
+        typeof require === 'function' && require('worker_threads');
+    WorkerClass = WorkerThreads.Worker;
+} catch(e) {} // eslint-disable-line
 
 export {WorkerClass};
